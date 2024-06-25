7f3e9b77c2e7def2d52ebe7ef8d6a03370939916
Danil <grechkindanil322@gmail.com>
2024-06-25 23:32:22 +0200

    add favicon
     Please enter the commit message for your changes. Lines starting
    
    fix readme
    
    fix readme
    
    created navBarMenu in modules

--- src/app/layouts/MainHeader/MainHeader.tsx
+++ src/app/layouts/MainHeader/MainHeader.tsx
@@ -1,13 +1,17 @@
-import Button from "@/app/ui/Button";
+"use client";
+import { NavigationContext } from "@/app/modules/navBar/NavigationContext";
+import Button from "@/app/ui/Buttons/Button";
+import { useContext } from "react";
 import { IoHome, IoMenu } from "react-icons/io5";
 import styles from "./MainHeader.module.css";
 
 const MainHeader = () => {
+    const { setIsNavigationActive } = useContext(NavigationContext);
     return (
         <header className={styles.header}>
             <section className={styles.wrapperHeaderContent}>
                 <div style={{ width: "33%" }}>
-                    <Button text={"Меню"} />
+                    <Button onClick={() => setIsNavigationActive(prev => !prev)} text={"Меню"} />
                 </div>
                 <a href="/" style={{ padding: "12px" }}>
                     <IoHome className={styles.iconHomePage} />
