b4053331f87b4c97e29d24b92c0802347576f887
Danil <grechkindanil322@gmail.com>
2024-07-06 22:25:55 +0200

    format date in companyCard

--- src/app/modules/companyCard/components/CompanyDetails.tsx
+++ src/app/modules/companyCard/components/CompanyDetails.tsx
@@ -1,20 +1,47 @@
 import { Company } from "@/types/company/company";
+import { format, toZonedTime } from "date-fns-tz";
+import Link from "next/link";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import styles from "../CompanyCard.module.css";
-
 interface Props {
     company: Company;
 }
 
+const RussianTime = (dateString: string) => {
+    const timeZone = "Europe/Moscow";
+    const date = new Date(dateString);
+    const zonedDate = toZonedTime(date, timeZone);
+    const formattedDate = format(zonedDate, "dd.MM.yyyy HH:mm:ss", { timeZone });
+
+    return formattedDate;
+};
+
 const CompanyDetails = ({ company }: Props) => {
+    const generateColor = (text: string) => {
+        if (text === "Легко") return "#7bfd01";
+        if (text === "Средне") return "#eec200";
+        if (text === "Сложно") return "#ee2400";
+    };
     return (
         <div className={styles.companyCard}>
             <section className={styles.wrapperSectiion}>
                 <div className={styles.point}>Название компании: {company.companyName}</div>
-                {company.linkVacancy && <div className={styles.point}>Ссылка на вакансию: {company.linkVacancy}</div>}
+                {company.linkVacancy && (
+                    <div className={styles.point}>
+                        Ссылка на вакансию:{" "}
+                        <Link className={styles.linkVacancy} href={company.linkVacancy}>
+                            {company.linkVacancy}
+                        </Link>
+                    </div>
+                )}
                 {company.description && <div className={styles.point}>Описание: {company.description}</div>}
-                <div className={styles.point}>Сложность: {company.difficulty}</div>
+                <div className={styles.point}>
+                    Сложность:
+                    <span style={{ color: generateColor(company.difficulty), fontWeight: "600" }}>
+                        &nbsp;{company.difficulty}
+                    </span>
+                </div>
                 <div className={styles.point}>Формат: {company.typeOfInterview}</div>
                 <div className={styles.point}>Лайфкодинг: {company.liveCoding}</div>
             </section>
@@ -34,7 +61,9 @@ const CompanyDetails = ({ company }: Props) => {
                     </SyntaxHighlighter>
                 </div>
             )}
-            <div className={styles.wrapperSectiion}>Дата создания: {company.createdAt}</div>
+            {company.createdAt && (
+                <div className={styles.wrapperSectiion}>Дата создания: {RussianTime(company.createdAt)}</div>
+            )}
         </div>
     );
 };
