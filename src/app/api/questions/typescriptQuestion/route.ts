dc7ef1e4a0759d586b0c671212ea07c94418d078
Danil <grechkindanil322@gmail.com>
2024-07-14 22:41:59 +0200

    fix route

--- src/app/api/questions/typescriptQuestion/route.ts
+++ src/app/api/questions/typescriptQuestion/route.ts
@@ -5,9 +5,9 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-    const { question, answer }: Question = await request.json();
+    const { question, answer, sliceOfCode, links, status }: Question = await request.json();
     await connectAuthMongoDB();
-    await TypescriptQuestionModel.create({ question, answer });
+    await TypescriptQuestionModel.create({ question, answer, sliceOfCode, links, status: status || "PENDING" });
     return NextResponse.json({ message: "TYPESCRIPT Question Created" }, { status: 201 });
 }
 
