fe608b05d1ed4cc92878740f0f746bdd1804db0c
Danil <grechkindanil322@gmail.com>
2024-07-18 21:43:00 +0200

    add mainPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+import homeImage from "../../public/homeImage.png";
+import styles from "./page.module.css";
+
 export default function Home() {
-    return <div style={{ flex: "1 1 auto" }}></div>;
+    return (
+        <div className={styles.mainWrapper}>
+            <div className={styles.content}>
+                <Image src={homeImage} alt="decoration" />
+                <section className={styles.flexContainer}>
+                    <h1 className={styles.title}>
+                        Добро пожаловать в сервис <br /> для подготовки к собеседованиям
+                    </h1>
+                    <Link className={styles.linkStart} href="questionsPage/allQuestions">
+                        Начать подготовку
+                    </Link>
+                </section>
+            </div>
+        </div>
+    );
 }
