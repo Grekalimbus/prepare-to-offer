2a3fe91d41a7599e9e816d1a1b74585d9a033947
Danil <grechkindanil322@gmail.com>
2024-07-21 15:37:51 +0200

    almost complete responsive design

--- src/frontend/layouts/MainHeader/MainHeader.tsx
+++ src/frontend/layouts/MainHeader/MainHeader.tsx
@@ -2,6 +2,7 @@ import { useNavBar } from "@/app/store";
 import DefaultButton from "@/frontend/ui/Buttons/defaultButton/DefaultButton";
 import Link from "next/link";
 import { IoHome } from "react-icons/io5";
+import { LuMenuSquare } from "react-icons/lu";
 import styles from "./MainHeader.module.css";
 import ButtonsLogin from "./components/ButtonsLogin";
 
@@ -9,15 +10,18 @@ const MainHeader = () => {
     const { setIsNavBar } = useNavBar();
     return (
         <header className={styles.header}>
-            <div className={styles.menuWrapper}>
-                <DefaultButton onClick={setIsNavBar} text={"Меню"} />;
+            <div className={styles.flexContainer}>
+                <div className={styles.menuWrapper}>
+                    <DefaultButton onClick={setIsNavBar} text={"Меню"} />;
+                </div>
+                <Link href="/" className={styles.iconWrapper}>
+                    <IoHome className={styles.iconHomePage} />
+                </Link>
+                <div className={styles.wrapperButtonsLogin}>
+                    <ButtonsLogin />
+                </div>
             </div>
-            <Link href="/" className={styles.iconWrapper}>
-                <IoHome className={styles.iconHomePage} />
-            </Link>
-            <div className={styles.wrapperButtonsLogin}>
-                <ButtonsLogin />
-            </div>
+            <LuMenuSquare className={styles.iconNav} onClick={() => setIsNavBar()} />
         </header>
     );
 };
